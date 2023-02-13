f08a10b2a9098c3a966aca44dace14460ad4e644
Brad Johnson <bradsk88@gmail.com>
2023-02-13 10:02:29 -0600

    New messaging design

--- src/components/Hub.tsx
+++ src/components/Hub.tsx
@@ -4,11 +4,19 @@ import {Connection} from "../common/connection";
 import {Button, Card, CardActions, CardContent, List, ListItem, ListSubheader} from "@mui/material";
 import './Hub.css';
 
+interface HubConnection extends Connection {
+    isRunning?: boolean; // TODO: Determine this on page load (ask the extension itself)
+}
+
+function getVariant(h: HubConnection): "text" | "contained" | "outlined" | undefined {
+    return h.isRunning ? "text" : "contained"
+}
+
 const debug = false;
 
 const Hub = () => {
     // TODO: Store extension connection names, icons, etc.
-    const [connections, setConnections] = useState<Connection[]>([]);
+    const [connections, setConnections] = useState<HubConnection[]>([]);
     const [checked, setChecked] = useState<boolean>(false);
 
     useEffect(() => {
@@ -17,7 +25,7 @@ const Hub = () => {
                 {
                     action: "get_connections",
                 },
-                (c: Connection[]) => {
+                (c: HubConnection[]) => {
                     setConnections(c);
                 },
             );
@@ -35,30 +43,56 @@ const Hub = () => {
                 <h1>Firefly III Extension Hub</h1>
                 <List>
                     <ListSubheader>Connections:</ListSubheader>
-                {connections?.length === 0 &&
-                    <ListItem>No connections found. Try reloading your scraper extension(s) and then reload this page.</ListItem>
-                }
-                {connections.map(c => (
-                    <ListItem className={"connection"}>
-                        <div>{c.name}</div>
-                        <div className={"spacer"}></div>
-                        <Button
-                            variant={"contained"}
-                            onClick={() => {
-                                const port = chrome.runtime.connect(c.id);
-                                port.postMessage({
-                                    action: "request_auto_run",
-                                })
-                            }}
-                        >Auto-Run</Button>
-                    </ListItem>
-                ))}
+                    {connections?.length === 0 &&
+                        <ListItem>No connections found. Try reloading your scraper extension(s) and then reload this
+                            page.</ListItem>
+                    }
+                    {connections.map(c => (
+                        <ListItem className={"connection"}>
+                            <div>{c.name}</div>
+                            <div className={"spacer"}></div>
+                            <Button
+                                variant={getVariant(c)}
+                                onClick={async () => {
+                                    if (c.isRunning) {
+                                        await chrome.runtime.sendMessage(c.id, {
+                                            action: "cancel_auto_run",
+                                        })
+                                        setConnections(connections.map(v => {
+                                            if (v.id === c.id) {
+                                                v.isRunning = false;
+                                            }
+                                            return v;
+                                        }))
+                                    } else {
+                                        await chrome.runtime.sendMessage(c.id, {
+                                            action: "request_auto_run",
+                                        })
+                                        setConnections(connections.map(v => {
+                                            if (v.id === c.id) {
+                                                v.isRunning = true;
+                                            }
+                                            return v;
+                                        }))
+                                    }
+                                }}
+                            >
+                                {c.isRunning
+                                    ? <span>Cancel</span>
+                                    : <span>Auto-Run</span>
+                                }
+                            </Button>
+                        </ListItem>
+                    ))}
                 </List>
             </CardContent>
             <CardActions>
                 <Button variant={"contained"} disabled={connections?.length === 0} onClick={() => {
                     connections.forEach(
-                        connection => {
+                        async connection => {
+                            await chrome.runtime.sendMessage(connection.id, {
+                                action: "request_auto_run",
+                            })
                             // TODO: Add "cancel auto run" button
                             const port = chrome.runtime.connect(connection.id);
                             port.postMessage({
@@ -66,10 +100,15 @@ const Hub = () => {
                             })
                         }
                     );
+                    setConnections(connections.map(v => {
+                        v.isRunning = true;
+                        return v;
+                    }))
                     chrome.storage.local.set({
                         firefly_iii_last_run: new Date(),
                     })
-                }}>Run auto-export for all connections
+                }}>
+                    Run auto-export for all connections
                 </Button>
 
                 <Button onClick={() => {
